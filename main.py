61b4c978116dc24ee734dd1ff22af9dee6a0f80a
bellinel <artursimoncik@gmail.com>
2025-10-14 22:22:24 +0300

    Update for rekr

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def main():
     
     await telethon_client.start(phone=PHONE_NUMBER)
     await register_topic_listener(telethon_client, TOPIC_MAP, AsyncSessionLocal, bot)
-    await register_simple_edit_listener(telethon_client, -1002658129391)
+    await register_simple_edit_listener(telethon_client, -1002658129391, bot)
    
     asyncio.create_task(monitor_and_cleanup(telethon_client, AsyncSessionLocal, bot))
     asyncio.create_task(check_and_delete_duplicates(telethon_client, -1002658129391, bot, TOPIC_MAP))
